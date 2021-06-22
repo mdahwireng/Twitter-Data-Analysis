9f15612d6551602f6405f87b1228ac817260f823
M D Ahwireng <kaaymike@hotmail.co.uk>
2021-06-22 17:06:53 +0000

    Completes find_source

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -63,8 +63,7 @@ class TweetDfExtractor:
         return created_at
 
     def find_source(self)->list:
-        source = 
-
+        source = [i['source'] for i in self.tweets_list]
         return source
 
     def find_screen_name(self)->list:
