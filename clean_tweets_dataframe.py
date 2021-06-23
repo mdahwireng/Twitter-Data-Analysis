e3489179a84b7affe3744d2ee084e0f387a89e86
M D Ahwireng <kaaymike@hotmail.co.uk>
2021-06-23 05:00:18 +0000

    Adds lines between methods

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -17,6 +17,7 @@ class Clean_Tweets:
         df = df[df['polarity'] != 'polarity']
         
         return df
+
     def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
         """
         drop duplicate rows
@@ -24,6 +25,7 @@ class Clean_Tweets:
         df.drop_duplicates(inplace=True)
         
         return df
+        
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
